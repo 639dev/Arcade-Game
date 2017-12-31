580ca29a0d31858482eceb85743ac23301dc63d8
639dev <gharood_3@hotmail.com>
2017-12-31 21:08:07 +0300

    add the ability to choose a character

--- js/app.js
+++ js/app.js
@@ -2,26 +2,30 @@ var level = 1;
 levelUpSound = new sound("audio/level_up.wav");
 loseSound = new sound("audio/lose_sound.wav");
 
+//listen to buttons clicks to change the player character
+$('.character-image').on('click', function(e) {
+    var img_button = document.getElementById(jQuery(this).attr("id")); //button
+    var src = $(this).find('img').attr('src')
+    player.sprite = src;
+    $('#char-list').toggle();
+});
 // Enemies our player must avoid
 var Enemy = function() {
     this.sprite = 'images/enemy-bug.png';
-    var myArray = [60,130,220];
-    var rand = myArray[Math.floor(Math.random() * myArray.length)];
+    var rand = generateRandomEnimies(); // generate random y value
     this.x = -30;
     this.y = rand;
-    this.speed = (Math.floor(Math.random() * 150) + 100);
+    this.speed = (Math.floor(Math.random() * 150) + 100); //generate random speed
 };
 
 
 // Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
-    this.x += this.speed*dt;
-    if(this.x > 500)
-    {
-        var myArray = [60,130,220];
-        var rand = myArray[Math.floor(Math.random() * myArray.length)];
-        this.y = rand; 
-        this.x =-30;
+    this.x += this.speed * dt;
+    if (this.x > 500) {
+        var rand = generateRandomEnimies(); //generate enimies come from diffrent y axis
+        this.y = rand;
+        this.x = -30;
     }
 
 };
@@ -30,78 +34,63 @@ Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
-Enemy.prototype.increaseEnemies = function(level){
+//generate number of enimies as current level
+Enemy.prototype.increaseEnemies = function(level) {
     allEnemies = [];
-    for(var i = 0 ;i<level;i++){
+    for (var i = 0; i < level; i++) {
         allEnemies.push(new Enemy());
     }
 };
 
 
-var Player = function(){
+var Player = function() {
     this.sprite = 'images/char-boy.png';
-    this.x =  200;
-    this.y =  380;
+    this.x = 200;
+    this.y = 380;
 };
 
 Player.prototype.update = function() {
     player.checkCollision();
 };
 
-Player.prototype.checkCollision = function(){
-    for(var i =0;i<allEnemies.length;i++)
-    {
-        if (player.x < allEnemies[i].x + 171/3 &&
-            player.x + 171/3 > allEnemies[i].x &&
-            player.y < allEnemies[i].y + 101/3 &&
-            player.y + 101/3 > allEnemies[i].y) {
-                loseSound.play();
-                level = 1;
-                $('#level').html('Level '+level);
-                Enemy.prototype.increaseEnemies(level);
-                player.x = 200;
-                player.y = 380;
-            }
+//choosen 171 & 101 based on the width of the pngs
+//ref: https://stackoverflow.com/questions/13916966/adding-collision-detection-to-images-drawn-on-canvas
+Player.prototype.checkCollision = function() {
+    for (var i = 0; i < allEnemies.length; i++) {
+        if (player.x < allEnemies[i].x + 171 / 3 &&
+            player.x + 171 / 3 > allEnemies[i].x &&
+            player.y < allEnemies[i].y + 101 / 3 &&
+            player.y + 101 / 3 > allEnemies[i].y) {
+            reset();
+        }
     }
 };
 
-Player.prototype.handleInput = function(e){
-    if(e == 'left'){
-        if( this.x == 0 ){
+Player.prototype.handleInput = function(e) {
+    if (e == 'left') {
+        if (this.x == 0) {
             this.x = this.x;
-        }
-        else{
+        } else {
             this.x -= 100;
         }
-    }
-    else if(e == 'up'){
-        if( this.y < 80){
+    } else if (e == 'up') {
+        if (this.y < 80) {
             this.y = this.y;
-            level++;
-            $('#level').html('Level '+level);
-            player.x = 200;
-            player.y = 380;
-            levelUpSound.play();
-            Enemy.prototype.increaseEnemies(level);
-        } 
-        else {
+            levelUp();
+        } else {
             this.y -= 80;
         }
-    }
-    else if(e == 'right'){
-        if( this.x == 400 ){
+    } else if (e == 'right') {
+        if (this.x == 400) {
             this.x = this.x;
-        }
-        else{
+        } else {
             this.x += 100;
         }
-    }
-    else{
-        if( this.y == 380){
+    } else {
+        if (this.y == 380) {
             this.y = this.y;
             console.log("ooops");
-        } 
-        else {
+        } else {
             this.y += 80;
         }
     }
@@ -114,7 +103,7 @@ Player.prototype.render = function() {
 
 
 var allEnemies = [new Enemy()];
-var player = new Player();
+window.player = new Player();
 
 
 // This listens for key presses and sends the keys to your
@@ -132,8 +121,7 @@ document.addEventListener('keyup', function(e) {
 
 
 
-
-//w3school
+//ref: w3school
 function sound(src) {
     this.sound = document.createElement("audio");
     this.sound.src = src;
@@ -141,10 +129,39 @@ function sound(src) {
     this.sound.setAttribute("controls", "none");
     this.sound.style.display = "none";
     document.body.appendChild(this.sound);
-    this.play = function(){
+    this.play = function() {
         this.sound.play();
     }
-    this.stop = function(){
+    this.stop = function() {
         this.sound.pause();
     }
+}
+
+//ref: https://stackoverflow.com/questions/4550505/getting-a-random-value-from-a-javascript-array
+function generateRandomEnimies() {
+    var myArray = [60, 130, 220];
+    var rand = myArray[Math.floor(Math.random() * myArray.length)];
+    return rand;
+}
+
+//when the payer fail, the level will be reset to 1
+function reset() {
+    loseSound.play();
+    level = 1;
+    resetPlayer();
+}
+
+// level++ then reposition the player
+function levelUp() {
+    levelUpSound.play();
+    level++;
+    resetPlayer();
+}
+
+// reposition the player
+function resetPlayer() {
+    $('#level').html('Level ' + level);
+    player.x = 200;
+    player.y = 380;
+    Enemy.prototype.increaseEnemies(level);
 }
--- js/engine.js
+++ js/engine.js
@@ -27,7 +27,6 @@ var Engine = (function(global) {
     canvas.width = 505;
     canvas.height = 606;
     doc.body.appendChild(canvas);
-    console.log("ctx created");
 
     /* This function serves as the kickoff point for the game loop itself
      * and handles properly calling the update and render methods.
@@ -174,7 +173,11 @@ var Engine = (function(global) {
         'images/water-block.png',
         'images/grass-block.png',
         'images/enemy-bug.png',
-        'images/char-boy.png'
+        'images/char-boy.png',
+        'images/char-cat-girl.png',
+        'images/char-horn-girl.png',
+        'images/char-pink-girl.png',
+        'images/char-princess-girl.png'
     ]);
     Resources.onReady(init);
 
